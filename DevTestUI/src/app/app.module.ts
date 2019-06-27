0ff9120479f720807ff55736d37011915b339196
Manish Malhotra <Manish.Malhotra@environment.nsw.gov.au>
2019-06-27 16:19:04 +1000

    Added xUnitTesting Project

--- DevTestUI/src/app/app.module.ts
+++ DevTestUI/src/app/app.module.ts
@@ -3,21 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employee/employee.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material';
 import {MatNativeDateModule} from '@angular/material';
 import {MatCardModule} from '@angular/material';
-import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EmployeeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
---
+++ xUnitTest.Tests/OccupationControllerShould.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace xUnitTest.Tests
+{
+    public class OccupationControllerShould
+    {
+        [Fact]
+        public void Test1()
+        {
+             
+        }
+    }
+}
