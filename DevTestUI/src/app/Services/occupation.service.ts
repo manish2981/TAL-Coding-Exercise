ada92d9e1c9366539a5347ec849b9ab78fc4a00d
Manish Malhotra <Manish.Malhotra@environment.nsw.gov.au>
2019-06-27 17:11:03 +1000

    Added Comments

--- DevTestUI/src/app/Services/occupation.service.ts
+++ DevTestUI/src/app/Services/occupation.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { ROOT_URL } from 'src/Models/Config';
 import { Occupation } from 'src/Models/Occupation';
 import { Observable } from 'rxjs';
+import { CodeNode } from 'source-list-map';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,55 @@ export class OccupationService {
   constructor(private http: HttpClient) { }
 
   getOccupationList() {
-    return this.http.get<Occupation[]>(ROOT_URL + 'occupation/GetOccupations');
+     return this.http.get<Occupation[]>(ROOT_URL + 'occupation/GetOccupations');
+
+      // Below is the commented Code
+      // Values from API will come in below json format
+      
+      // var options2 =[
+      //   {
+      //   occupationid : '1',
+      //   occupation : 'Cleaner',
+      //   Rating: 'Light Manual',
+      //   RatingId:'3',
+      //   Factor:'1.50'
+      //   },
+      //   {
+      //     occupationid : '2',
+      //     occupation : 'Doctor',
+      //     Rating: 'Professional',
+      //     RatingId:'1',
+      //     Factor:'1.0'
+      //   },
+      //   {
+      //     occupationid : '3',
+      //     occupation : 'Author',
+      //     Rating: 'White Collar',
+      //     RatingId:'2',
+      //     Factor:'1.25'
+      //   },
+      //   {
+      //     occupationid : '4',
+      //     occupation : 'Farmer',
+      //     Rating: 'Heavy Manual',
+      //     RatingId:'4',
+      //     Factor:'1.75'
+      //   },
+      //   {
+      //     occupationid : '5',
+      //     occupation : 'Mechanic',
+      //     Rating: 'Heavy Manual',
+      //     RatingId:'4',
+      //     Factor:'1.75'
+      //   },
+      //   {
+      //     occupationid : '6',
+      //     occupation : 'Florist',
+      //     Rating: 'Light Manual',
+      //     RatingId:'3',
+      //     Factor:'1.50'
+      //   }];
+      //   return options2;
   }
 
 }
--- DevTestUI/src/app/app.component.ts
+++ DevTestUI/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators  } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators, FormArray  } from '@angular/forms';
 import { OccupationService } from './Services/occupation.service';
+import { Occupation } from 'src/Models/Occupation';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,51 +13,52 @@ export class AppComponent implements OnInit{
   title = 'DevTestUI';
   isEditable: boolean = true;
   customerForm: FormGroup;
-
-  public options2 =[
-    {
-    occupationid : '1',
-    occupation : 'Cleaner',
-    Rating: 'Light Manual',
-    RatingId:'3',
-    Factor:'1.50'
-    },
-    {
-      occupationid : '2',
-      occupation : 'Doctor',
-      Rating: 'Professional',
-      RatingId:'1',
-      Factor:'1.0'
-    },
-    {
-      occupationid : '3',
-      occupation : 'Author',
-      Rating: 'White Collar',
-      RatingId:'2',
-      Factor:'1.25'
-    },
-    {
-      occupationid : '4',
-      occupation : 'Farmer',
-      Rating: 'Heavy Manual',
-      RatingId:'4',
-      Factor:'1.75'
-    },
-    {
-      occupationid : '5',
-      occupation : 'Mechanic',
-      Rating: 'Heavy Manual',
-      RatingId:'4',
-      Factor:'1.75'
-    },
-    {
-      occupationid : '6',
-      occupation : 'Florist',
-      Rating: 'Light Manual',
-      RatingId:'3',
-      Factor:'1.50'
-    }];
-  selected2 = this.options2[0].occupationid;
+  occupationList: any;
+  occupationList1: FormArray;
+  // public options2 =[
+  //   {
+  //   occupationid : '1',
+  //   occupation : 'Cleaner',
+  //   Rating: 'Light Manual',
+  //   RatingId:'3',
+  //   Factor:'1.50'
+  //   },
+  //   {
+  //     occupationid : '2',
+  //     occupation : 'Doctor',
+  //     Rating: 'Professional',
+  //     RatingId:'1',
+  //     Factor:'1.0'
+  //   },
+  //   {
+  //     occupationid : '3',
+  //     occupation : 'Author',
+  //     Rating: 'White Collar',
+  //     RatingId:'2',
+  //     Factor:'1.25'
+  //   },
+  //   {
+  //     occupationid : '4',
+  //     occupation : 'Farmer',
+  //     Rating: 'Heavy Manual',
+  //     RatingId:'4',
+  //     Factor:'1.75'
+  //   },
+  //   {
+  //     occupationid : '5',
+  //     occupation : 'Mechanic',
+  //     Rating: 'Heavy Manual',
+  //     RatingId:'4',
+  //     Factor:'1.75'
+  //   },
+  //   {
+  //     occupationid : '6',
+  //     occupation : 'Florist',
+  //     Rating: 'Light Manual',
+  //     RatingId:'3',
+  //     Factor:'1.50'
+  //   }];
+  // selected2 = this.occupationList[0].occupationid;
   constructor(private fb: FormBuilder, private dataService: OccupationService) {
     this.createForm();
   }
@@ -75,15 +78,14 @@ export class AppComponent implements OnInit{
 
 
   ngOnInit() {
-        var res = this.dataService.getOccupationList().subscribe(res=>{
-          
-        });
+        this.occupationList = this.dataService.getOccupationList().subscribe(res=>res);
+        console.log(this.occupationList);
   }
 
   calculatePremium(event: any) {
-            var filteredItems = Object.assign([], this.options2).filter(
-              item => item.occupationid.toLowerCase().indexOf(event.value) > -1);
-            console.log('Test',filteredItems[0]['Factor']);
+            var filteredItems = Object.assign([], this.occupationList).filter(
+                                item => item.occupationid.toLowerCase().indexOf(event.value) > -1);
+            console.log(filteredItems);
             var totalPremium = (this.customerForm.get('sumInsured').value * this.customerForm.get('age').value * filteredItems[0]['Factor'])/(1000*12);
             this.customerForm.controls['totalPremium'].setValue(totalPremium);
     }
