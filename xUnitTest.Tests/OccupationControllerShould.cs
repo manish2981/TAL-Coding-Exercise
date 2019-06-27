155c89d5d789ed131f614ab0356173ecff74b6f9
Manish Malhotra <Manish.Malhotra@environment.nsw.gov.au>
2019-06-27 16:37:26 +1000

    Updated xUnitTest Project

--- xUnitTest.Tests/OccupationControllerShould.cs
+++ xUnitTest.Tests/OccupationControllerShould.cs
@@ -5,10 +5,23 @@ namespace xUnitTest.Tests
 {
     public class OccupationControllerShould
     {
-        [Fact]
-        public void Test1()
-        {
-             
-        }
+         OccupationController _controller;
+        // [Fact]
+        // public async Task TestGetOccupationItemsAsync()
+        // {
+        //     // Arrange
+        //     var dbContext = DbContextMocker.GetWideWorldImportersDbContext(nameof(TestGetOccupationItemsAsync));
+        //     var controller = new OccupationController(null, dbContext);
+
+        //     // Act
+        //     var response = await controller.GetStockItemsAsync() as ObjectResult;
+        //     var value = response.Value as IPagedResponse<StockItem>;
+
+        //     dbContext.Dispose();
+
+        //     // Assert
+        //     Assert.False(value.DidError);
+        // }
+
     }
 }
